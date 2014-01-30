1c6ec788346d3188274bc863274e9c7c01f161c6
John Ptacek <jptacek@live.com>
2014-01-30 06:59:38 -0600

    Add stub for artists page

--- MileOfMusic/www/js/Controllers/artistsCtrl.js
+++ MileOfMusic/www/js/Controllers/artistsCtrl.js
@@ -1,28 +1,13 @@
 angular.module('mileOfMusicApp', ['fsCordova'])
     .controller('artistsController',
     function($scope, CordovaService) {
-        $scope.kitty = "foo";
+        $scope.showArtists = false;
         CordovaService.ready.then(function() {
-            $scope.performers ={
-                artists: [
-                    {
-                        "artistName": "California Wives",
-                        "twitter": "californiawives",
-                        "instagram": "",
-                        "facebook": "https://www.facebook.com/CaliforniaWives",
-                        "description": "",
-                        "url": "http://californiawives.com/"
-                    },
-                    {
-                        "artistName": "Travelling Suitcases",
-                        "twitter": "trvlingsuitcase",
-                        "instagram": "",
-                        "facebook": "https://www.facebook.com/TheTravelingSuitcase",
-                        "description": "",
-                        "url": "http://californiawives.com/"
-                    }
-                ]
-            };
+            $scope.performers =artistsData;
+
+            $scope.displayArtists = function() {
+                $scope.showArtists = true;
+            }
         });
     });
 
