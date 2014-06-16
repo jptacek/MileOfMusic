1cdaac68d6d6a73132da52431d981ff5e5b4c2e7
John Ptacek <jptacek@live.com>
2014-06-16 14:39:37 -0500

    more js work

--- MileOfMusic/www/js/Controllers/myScheduleCtr2.js
+++ MileOfMusic/www/js/Controllers/myScheduleCtr2.js
@@ -1,11 +1,34 @@
 mileOfMusicApp.controller('mySchedule2',
-    function ($scope, $log, $routeParams,  CordovaService) {
+    function ($scope, $log, $routeParams, myScheduleData, CordovaService) {
 
-        alert('stat me i[');
-        $scope.foo = "test";
-        CordovaService.ready.then(function () {
-            alert('getting data');
+        CordovaService.ready.then(function() {
+            myScheduleData.getMySchedule().then(function (result) {
+                $scope.mySchedule = result;
+            });
 
+
+            $scope.removeFavorite = function (concertId) {
+                try {
+                    var result = myScheduleData.removeConcertFromMySchedule(concertId);
+                    if (result) {
+                        toastr["success"]("Concert has been removed to your schedule.", "Success");
+                        myScheduleData.getMySchedule().then(function (result) {
+                            $scope.mySchedule = result;
+                        });
+                    }
+                    else {
+                        toastr["info"]("Concert was not in your schedule.", "Already Removed");
+                    }
+                }
+                catch (err) {
+                    toastr["error"](err.message, "Error");
+                }
+            };
+
+            $scope.clearSchedule = function () {
+                myScheduleData.clearSavedBookmarks();
+                $scope.mySchedule = [];
+            };
 
         });
     });
