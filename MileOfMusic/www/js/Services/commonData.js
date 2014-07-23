f91657fc4b8c465d6629819b1bac7e8cf3e87f71
John Ptacek <jptacek@live.com>
2014-07-23 15:22:10 -0500

    another try

--- MileOfMusic/www/js/Services/commonData.js
+++ MileOfMusic/www/js/Services/commonData.js
@@ -87,67 +87,72 @@
 
         $.each(data, function (i, item) {
             var localItem = item;
-            $.ajax({
-                url: localItem[photoUrlLocalProperty],
-                type: 'HEAD',
-                error: function () {
-                    localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
+            localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
+            //$.ajax({
+            //    url: localItem[photoUrlLocalProperty],
+            //    type: 'HEAD',
+            //    error: function () {
+            //        localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
                     
-                    //localItem[photoUrlProperty] = localItem[photoUrlRemoteProperty];
-                    //try {
-                    //    var fileTransfer = new FileTransfer();
-                    //    var uri = encodeURI(localItem[photoUrlRemoteProperty]);
+            //        //localItem[photoUrlProperty] = localItem[photoUrlRemoteProperty];
+            //        //try {
+            //        //    var fileTransfer = new FileTransfer();
+            //        //    var uri = encodeURI(localItem[photoUrlRemoteProperty]);
                         
-                    //    try
-                    //    {
-                    //        fileTransfer.download(
-                    //            uri,
-                    //            "cdvfile://localhost/persistent/" + targetDirectory + "/" + localItem[idProperty] + ".jpg",
-                    //            function (entry) {
-                    //                localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-                    //            },
-                    //            function (error) {
-                    //                $log.error(error);
-                    //                fileTransfer.download(
-                    //                   uri,
-                    //                   targetDirectory + "/" + localItem[idProperty] + ".jpg",
-                    //                   function (entry) {
-                    //                       localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-                    //                   },
-                    //                   function (error) {
-                    //                       $log.error(error);
-                    //                   }
-                    //               );
-                    //            }
-                    //        );
-                    //    }
-                    //    catch (err) { }
-                    //}
-                    //catch (err) { $log.error(err); }
-                },
-                success: function () {
-                    localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-                }, complete: function () {
-                    if (localItem[photoUrlProperty] == null) localItem[photoUrlProperty] = ""
-                }
-            });
+            //        //    try
+            //        //    {
+            //        //        fileTransfer.download(
+            //        //            uri,
+            //        //            "cdvfile://localhost/persistent/" + targetDirectory + "/" + localItem[idProperty] + ".jpg",
+            //        //            function (entry) {
+            //        //                localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
+            //        //            },
+            //        //            function (error) {
+            //        //                $log.error(error);
+            //        //                fileTransfer.download(
+            //        //                   uri,
+            //        //                   targetDirectory + "/" + localItem[idProperty] + ".jpg",
+            //        //                   function (entry) {
+            //        //                       localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
+            //        //                   },
+            //        //                   function (error) {
+            //        //                       $log.error(error);
+            //        //                   }
+            //        //               );
+            //        //            }
+            //        //        );
+            //        //    }
+            //        //    catch (err) { }
+            //        //}
+            //        //catch (err) { $log.error(err); }
+            //    },
+            //    success: function () {
+            //        localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
+            //    }, complete: function () {
+            //        if (localItem[photoUrlProperty] == null) localItem[photoUrlProperty] = ""
+            //    }
+            //});
         });
 
-        var attempts = 0;
-        var interval = setInterval(function () {
-            var allAssigned = true;
-            $.each(data, function (i, item) {
-                if (item[photoUrlProperty] == undefined) {
-                    allAssigned = false;
-                    return false;
-                }
-            });
-            attempts++;
-            if (allAssigned || attempts >= 50) {
-                clearInterval(interval);
-                deferred.resolve();
-            }
+        setInterval(function () {
+            deferred.resolve();
         }, 100);
+
+        //var attempts = 0;
+        //var interval = setInterval(function () {
+        //    var allAssigned = true;
+        //    $.each(data, function (i, item) {
+        //        if (item[photoUrlProperty] == undefined) {
+        //            allAssigned = false;
+        //            return false;
+        //        }
+        //    });
+        //    attempts++;
+        //    if (allAssigned || attempts >= 50) {
+        //        clearInterval(interval);
+        //        deferred.resolve();
+        //    }
+        //}, 100);
 
         return deferred.promise;
     };
