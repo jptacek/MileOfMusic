12a95610b2c88913b021c1b8919820b69145ac5d
John Ptacek <jptacek@live.com>
2014-07-27 10:18:20 -0500

    Change data tload

--- MileOfMusic/www/js/Services/commonData.js
+++ MileOfMusic/www/js/Services/commonData.js
@@ -1,80 +1,86 @@
-﻿mileOfMusicApp.factory('commonData', function ($http, $log, $q, appHelper) {
+﻿mileOfMusicApp.factory('commonData', function ($http, $log, $q, appHelper, notificationFactory) {
     var getRemoteData = function (storageKey, versionUrl, dataUrl, newDataCallback, preStoreDataCallback, getCachedDataCallback) {
         var deferred = $q.defer();
-
-        var dataKey = storageKey;
-        var versionKey = storageKey + "-version";
-        var versionDateKey = versionKey + "-date";
-
-        var lastVersionCheck = new Date(localStorage.getItem(versionDateKey));
-        var data = localStorage.getItem(dataKey);
-        //var dateCheck = new Date(new Date().getTime() - (12 * 60 * 60 * 1000)); // Check twice per day
-        var dateCheck = new Date(new Date().getTime() - (1 * 60 * 1000)); // Check every hours
-
-        if (data != null && lastVersionCheck != null && lastVersionCheck > dateCheck) {
-            var jsonData = null;
-            if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
-            if (jsonData == null) {
-                jsonData = JSON.parse(data);
-            }
-            deferred.resolve(jsonData);
+        if (navigator != null && navigator.network != null && navigator.network.connection != null && navigator.network.connection.type == Connection.NONE) {
+        //    if (navigator == null || navigator.network == null || navigator.network.connection == null || navigator.network.connection.type != Connection.NONE) {
+            notificationFactory.error("No network connection detected. Cannot display page without a connection.");
+            deferred.reject();
         }
         else {
-            $http.jsonp(versionUrl).then(function (result) {
-                var myVersion = localStorage.getItem(versionKey);
-                if (data == null || myVersion == null || JSON.parse(myVersion).Version != result.data.Version) {
 
-                    // New version, so go download new JSON
-                    $http.jsonp(dataUrl).then(function (dataResult) {
+            var dataKey = storageKey;
+            var versionKey = storageKey + "-version";
+            var versionDateKey = versionKey + "-date";
 
-                        if (newDataCallback != null) newDataCallback();
+            var lastVersionCheck = new Date(localStorage.getItem(versionDateKey));
+            var data = localStorage.getItem(dataKey);
+            //var dateCheck = new Date(new Date().getTime() - (12 * 60 * 60 * 1000)); // Check twice per day
+            var dateCheck = new Date(new Date().getTime() - (4 * 60 * 1000)); // Check every hours
 
-                        function finalizeData() {
-                            localStorage.setItem(versionKey, JSON.stringify(result.data));
-                            localStorage.setItem(versionDateKey, new Date().toString());
-                            localStorage.setItem(dataKey, JSON.stringify(dataResult));
-                            deferred.resolve(dataResult);
-                        }
+            if (data != null && lastVersionCheck != null && lastVersionCheck > dateCheck) {
+                var jsonData = null;
+                if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
+                if (jsonData == null) {
+                    jsonData = JSON.parse(data);
+                }
+                deferred.resolve(jsonData);
+            }
+            else {
+                $http.jsonp(versionUrl).then(function (result) {
+                    var myVersion = localStorage.getItem(versionKey);
+                    if (data == null || myVersion == null || JSON.parse(myVersion).Version != result.data.Version) {
 
-                        if (preStoreDataCallback == null) {
-                            finalizeData();
-                        }
-                        else {
-                            // preStoreDataCallback MUST be a promise
-                            preStoreDataCallback(dataResult.data).then(function () {
+                        // New version, so go download new JSON
+                        $http.jsonp(dataUrl).then(function (dataResult) {
+
+                            if (newDataCallback != null) newDataCallback();
+
+                            function finalizeData() {
+                                localStorage.setItem(versionKey, JSON.stringify(result.data));
+                                localStorage.setItem(versionDateKey, new Date().toString());
+                                localStorage.setItem(dataKey, JSON.stringify(dataResult));
+                                deferred.resolve(dataResult);
+                            }
+
+                            if (preStoreDataCallback == null) {
                                 finalizeData();
-                            }, function (e) { deferred.reject(); });
+                            }
+                            else {
+                                // preStoreDataCallback MUST be a promise
+                                preStoreDataCallback(dataResult.data).then(function () {
+                                    finalizeData();
+                                }, function (e) { deferred.reject(); });
+                            }
+                        }, function (e) { deferred.reject(); });
+                    }
+                    else {
+                        // Pull from cache
+                        localStorage.setItem(versionDateKey, new Date().toString());
+
+                        var jsonData = null;
+                        if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
+                        if (jsonData == null) {
+                            jsonData = JSON.parse(data);
                         }
-                    }, function (e) { deferred.reject(); });
-                }
-                else {
-                    // Pull from cache
-                    localStorage.setItem(versionDateKey, new Date().toString());
-
-                    var jsonData = null;
-                    if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
-                    if (jsonData == null) {
-                        jsonData = JSON.parse(data);
+                        deferred.resolve(jsonData);
                     }
-                    deferred.resolve(jsonData);
-                }
-            }, function () {
-                var data = localStorage.getItem(dataKey);
-                if (data == null) {
-                    deferred.reject();
-                }
-                else {
-                    // Version get failed, but we have a cached version so just use that
-                    var jsonData = null;
-                    if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
-                    if (jsonData == null) {
-                        jsonData = JSON.parse(data);
+                }, function () {
+                    var data = localStorage.getItem(dataKey);
+                    if (data == null) {
+                        deferred.reject();
                     }
-                    deferred.resolve(jsonData);
-                }
-            });
+                    else {
+                        // Version get failed, but we have a cached version so just use that
+                        var jsonData = null;
+                        if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
+                        if (jsonData == null) {
+                            jsonData = JSON.parse(data);
+                        }
+                        deferred.resolve(jsonData);
+                    }
+                });
+            }
         }
-
         return deferred.promise;
     };
 
