7b94c184b5fa434457fb1ae51f66daaa140771ea
John Ptacek <jptacek@live.com>
2014-07-27 18:57:33 -0500

    get data when no network

--- MileOfMusic/www/js/Services/commonData.js
+++ MileOfMusic/www/js/Services/commonData.js
@@ -1,17 +1,19 @@
 ﻿mileOfMusicApp.factory('commonData', function ($http, $log, $q, appHelper, notificationFactory) {
     var getRemoteData = function (storageKey, versionUrl, dataUrl, newDataCallback, preStoreDataCallback, getCachedDataCallback) {
         var deferred = $q.defer();
+
+        var dataKey = storageKey;
+        var versionKey = storageKey + "-version";
+        var versionDateKey = versionKey + "-date";
+
         if (navigator !== undefined && navigator.network !== undefined && navigator.network.connection !== undefined && navigator.network.connection.type == Connection.NONE) {
         //    if (navigator == null || navigator.network == null || navigator.network.connection == null || navigator.network.connection.type != Connection.NONE) {
             alert('no netowrk');
-            alert('navigator != null ' + (navigator !== undefined).toString() );
-            alert('navigator.network != null ' + (navigator.network !== undefined ));
-            alert('navigator.network.connection != null  ' + (navigator.network.connection !== undefined ));
-            alert('navigator.network.connection.type  ' + navigator.network.connection.type);
             notificationFactory.error("No network connection detected. Cannot display page without a connection.");
             // Pull from cache
             localStorage.setItem(versionDateKey, new Date().toString());
 
+            var data = localStorage.getItem(dataKey);
             var jsonData = null;
             if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
             if (jsonData == null) {
@@ -21,14 +23,6 @@
         }
         else {
             alert(' netowrk');
-            alert('navigator != null ' + (navigator !== undefined).toString() );
-            alert('navigator.network != null ' + (navigator.network !== undefined ));
-            alert('navigator.network.connection != null  ' + (navigator.network.connection !== undefined ));
-            alert('navigator.network.connection.type  ' + navigator.network.connection.type);
-
-            var dataKey = storageKey;
-            var versionKey = storageKey + "-version";
-            var versionDateKey = versionKey + "-date";
 
             var lastVersionCheck = new Date(localStorage.getItem(versionDateKey));
             var data = localStorage.getItem(dataKey);
@@ -37,7 +31,9 @@
 
             if (data != null && lastVersionCheck != null && lastVersionCheck > dateCheck) {
                 var jsonData = null;
-                if (getCachedDataCallback != null) jsonData = getCachedDataCallback();
+                if (getCachedDataCallback != null) {
+                    jsonData = getCachedDataCallback();
+                }
                 if (jsonData == null) {
                     jsonData = JSON.parse(data);
                 }
@@ -51,7 +47,9 @@
                         // New version, so go download new JSON
                         $http.jsonp(dataUrl).then(function (dataResult) {
 
-                            if (newDataCallback != null) newDataCallback();
+                            if (newDataCallback != null) {
+                                newDataCallback();
+                            }
 
                             function finalizeData() {
                                 localStorage.setItem(versionKey, JSON.stringify(result.data));
@@ -112,71 +110,12 @@
         $.each(data, function (i, item) {
             var localItem = item;
             localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-            //$.ajax({
-            //    url: localItem[photoUrlLocalProperty],
-            //    type: 'HEAD',
-            //    error: function () {
-            //        localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-                    
-            //        //localItem[photoUrlProperty] = localItem[photoUrlRemoteProperty];
-            //        //try {
-            //        //    var fileTransfer = new FileTransfer();
-            //        //    var uri = encodeURI(localItem[photoUrlRemoteProperty]);
-                        
-            //        //    try
-            //        //    {
-            //        //        fileTransfer.download(
-            //        //            uri,
-            //        //            "cdvfile://localhost/persistent/" + targetDirectory + "/" + localItem[idProperty] + ".jpg",
-            //        //            function (entry) {
-            //        //                localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-            //        //            },
-            //        //            function (error) {
-            //        //                $log.error(error);
-            //        //                fileTransfer.download(
-            //        //                   uri,
-            //        //                   targetDirectory + "/" + localItem[idProperty] + ".jpg",
-            //        //                   function (entry) {
-            //        //                       localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-            //        //                   },
-            //        //                   function (error) {
-            //        //                       $log.error(error);
-            //        //                   }
-            //        //               );
-            //        //            }
-            //        //        );
-            //        //    }
-            //        //    catch (err) { }
-            //        //}
-            //        //catch (err) { $log.error(err); }
-            //    },
-            //    success: function () {
-            //        localItem[photoUrlProperty] = localItem[photoUrlLocalProperty];
-            //    }, complete: function () {
-            //        if (localItem[photoUrlProperty] == null) localItem[photoUrlProperty] = ""
-            //    }
-            //});
+
         });
 
         setInterval(function () {
             deferred.resolve();
         }, 100);
-
-        //var attempts = 0;
-        //var interval = setInterval(function () {
-        //    var allAssigned = true;
-        //    $.each(data, function (i, item) {
-        //        if (item[photoUrlProperty] == undefined) {
-        //            allAssigned = false;
-        //            return false;
-        //        }
-        //    });
-        //    attempts++;
-        //    if (allAssigned || attempts >= 50) {
-        //        clearInterval(interval);
-        //        deferred.resolve();
-        //    }
-        //}, 100);
 
         return deferred.promise;
     };
