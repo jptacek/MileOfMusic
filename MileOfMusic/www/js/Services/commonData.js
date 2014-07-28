0ca921ada10b64501959464d1668f989663bbfdc
John Ptacek <jptacek@live.com>
2014-07-28 06:09:56 -0500

    try again

--- MileOfMusic/www/js/Services/commonData.js
+++ MileOfMusic/www/js/Services/commonData.js
@@ -24,10 +24,8 @@
                     alert('Trying to set local data NEW ' + initialData);
                     var reader = new FileReader();
                     reader.onloadend = function(evt) {
-                        console.log("read success");
-                        console.log(evt.target.result);
                     };
-                    var textResult = reader.readAsText(initialData);
+                    var textResult = reader.readAsText('/'+initialData);
                     alert(textResult);
                     var dataResult = JSON.stringify(textResult);
                     jsonData = JSON.parse(dataResult);
