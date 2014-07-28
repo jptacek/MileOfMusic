60e5622d43f8c47a6379ad9c313092533c6fa8d8
jptacek <IluVBoots20!$GT>
2014-07-28 11:49:37 -0500

    update with error laert

--- MileOfMusic/www/js/Controllers/artistCtrl.js
+++ MileOfMusic/www/js/Controllers/artistCtrl.js
@@ -14,7 +14,7 @@ mileOfMusicApp.controller('artistController',
 
                 if (navigator != null && navigator.network != null &&
                     navigator.network.connection != null && navigator.network.connection.type != Connection.NONE) {
-
+                    alert('network - get data');
                     $.each(result, function(i, item) {
                         item.audio = $sce.trustAsHtml("<audio controls='controls' preload='none'><source src='" + item.musicUrl + "' /></audio>");
                     });
