3bd9f2c8edb95ceb377cb65561f1e5d2ca1e3796
John Ptacek <jptacek@live.com>
2014-07-28 07:11:31 -0500

    add new online/offline  handling

--- MileOfMusic/www/js/index.js
+++ MileOfMusic/www/js/index.js
@@ -36,8 +36,8 @@ var app = {
         var conn = checkConnection();
 
         app.receivedEvent('deviceready');
-        window.addEventListener('online', this.onOnline, false);
-        window.addEventListener('offline', this.onOffline, false);
+        document.addEventListener('online', this.onOnline, false);
+        document.addEventListener('offline', this.onOffline, false);
         if((navigator.network.connection.type).toUpperCase() == "NONE" &&
             (navigator.network.connection.type).toUpperCase() == "UNKNOWN") {
             this.onOffline();
